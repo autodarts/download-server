ca5d1f47903b77103382b577707108a499b8e9da
Timo Nolle <timonolle@gmail.com>
2023-02-09 14:39:54 +0100

    Add uvc hack endpoint

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	http.Handle("/", http.RedirectHandler("https://raw.githubusercontent.com/autodarts/releases/main/install.sh", 301))
+	http.Handle("/uvc", http.RedirectHandler("https://raw.githubusercontent.com/autodarts/releases/main/uvc/install.sh", 301))
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
