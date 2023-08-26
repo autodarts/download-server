5393239dce2dffaddb767ccd13cf0c008cc54080
Timo Nolle <timonolle@gmail.com>
2023-08-26 15:35:39 +0200

    Add handler for windows installer

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	http.Handle("/", http.RedirectHandler("https://raw.githubusercontent.com/autodarts/releases/main/install.sh", 301))
 	http.Handle("/uvc", http.RedirectHandler("https://raw.githubusercontent.com/autodarts/releases/main/uvc/install.sh", 301))
+	http.Handle("/win", http.RedirectHandler("https://raw.githubusercontent.com/autodarts/releases/main/install.ps1", 301))
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
